b7316bebc629838c989cedb8db8d431425f45e9c
gitachyut <linkinakd72@gmail.com>
2017-10-13 18:32:41 +0530

    initial setup for react reactstrap with webpack and scss

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// Import Main styles for this application
+import '../scss/style.scss'
+import App from './App'
+ReactDOM.render((
+  <App />
+), document.getElementById('root'));
